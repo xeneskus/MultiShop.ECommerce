5b6332145661d57ff4d3596a42ed4e04b981e430
xeneskus <xeneskus@gmail.com>
2024-04-02 02:48:05 +0300

    Added Product Entity

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -3,7 +3,7 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace MultiShop.Catalog.Entities
 {
-    public class Catalog
+    public class Category
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
