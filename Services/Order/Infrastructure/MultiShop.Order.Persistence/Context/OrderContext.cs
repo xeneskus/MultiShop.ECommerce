aad2bc594cdc7637d5b73b0e39d2df20cd04585d
xeneskus <xeneskus@gmail.com>
2024-04-05 15:42:25 +0300

    Added Order Context

---
+++ Services/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=NIRVANA\\SQLEXPRESS05;initial Catalog=MultiShopOrderDb;integrated Security=true;TrustServerCertificate=true;");
+
+        }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Ordering> Orderings { get; set; }
+    }
+}
+
