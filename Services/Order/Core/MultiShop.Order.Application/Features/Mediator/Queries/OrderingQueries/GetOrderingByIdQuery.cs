eb7b9eb611ab58282d0fbb40d9c507d7d159383e
xeneskus <xeneskus@gmail.com>
2024-04-05 16:13:26 +0300

    Added Ordering Queries

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/Mediator/Queries/OrderingQueries/GetOrderingByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Order.Application.Features.Mediator.Results.OrderingResults;
+
+namespace MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries
+{
+    public class GetOrderingByIdQuery : MediatR.IRequest<GetOrderingByIdQueryResult>
+    {
+        public int Id { get; set; }
+        public GetOrderingByIdQuery(int id)
+        {
+            Id = id;
+        }
+    }
+}
