0cddb1cb9f92778a3739648dc7b10962a37456a1
xeneskus <xeneskus@gmail.com>
2024-04-08 00:22:59 +0300

    Added Cargo Companies Api Controller For Cargo Web Api Layer

--- Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
+++ Services/Cargo/MultiShop.Cargo.WebApi/Program.cs
@@ -1,3 +1,9 @@
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.BusinessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.DataAccessLayer.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CargoContext>();
+builder.Services.AddScoped<ICargoCompanyDal, EfCargoCompanyDal>();
+builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
+
 
 var app = builder.Build();
 
