22d380fd972fb492d6fa8e60950a34aa61e827b0
xeneskus <xeneskus@gmail.com>
2024-04-08 17:28:54 +0300

    Added Baskets Api Controller For Basket Layer

--- Services/Basket/MultiShop.Basket/Program.cs
+++ Services/Basket/MultiShop.Basket/Program.cs
@@ -1,3 +1,6 @@
+using MultiShop.Basket.LoginServices;
+using MultiShop.Basket.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+
 
 var app = builder.Build();
 
