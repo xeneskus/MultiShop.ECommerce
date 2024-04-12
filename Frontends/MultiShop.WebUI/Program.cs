495006b970d3289e4e654bdbbf7095fafbd95816
xeneskus <xeneskus@gmail.com>
2024-04-12 19:19:22 +0300

    Uploaded images

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -2,7 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
+
+
+
 
 var app = builder.Build();
 
