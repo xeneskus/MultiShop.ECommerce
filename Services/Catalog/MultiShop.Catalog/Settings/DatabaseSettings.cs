f89d4bd1d9a6a488b8fc89bd665a302604feff12
xeneskus <xeneskus@gmail.com>
2024-05-30 21:01:08 +0300

    Added AutoMapper configurations for SpecialOffer and Added SpecialOfferCollectionName property to DatabaseSettings interface.

--- Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
+++ Services/Catalog/MultiShop.Catalog/Settings/DatabaseSettings.cs
@@ -6,8 +6,10 @@
         public string ProductCollectionName { get; set; }
         public string ProductDetailCollectionName { get; set; }
         public string ProductImageCollectionName { get; set; }
+        public string FeatureSliderCollectionName { get; set ; }
+        public string SpecialOfferCollectionName { get; set ; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
-        public string FeatureSliderCollectionName { get; set ; }
+
     }
 }
