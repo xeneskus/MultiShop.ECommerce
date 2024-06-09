cbcbf5b1ecb6c2ce8e6e691efbbf9fa1cf676063
xeneskus <xeneskus@gmail.com>
2024-06-09 23:17:24 +0300

    Fetching Related Products According to the Selected Category

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -4,14 +4,16 @@ namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
+            ViewBag.i = id;
             return View();
         }
-        public IActionResult ProductDetail()
-        {
-            return View();
 
+        public IActionResult ProductDetail(string id)
+        {
+            ViewBag.x = id;
+            return View();
         }
     }
 }
