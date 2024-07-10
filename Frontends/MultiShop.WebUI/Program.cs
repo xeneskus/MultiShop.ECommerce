18ed6a9fabe0ada624d93fe844f20033e5743eb9
xeneskus <xeneskus@gmail.com>
2024-07-10 21:25:46 +0300

    Added Login Service

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.WebUI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,7 +23,7 @@ builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
 
 
-
+builder.Services.AddScoped<ILoginService, LoginService>();
 
 var app = builder.Build();
 
---
+++ Frontends/MultiShop.WebUI/Services/ILoginService.cs
@@ -0,0 +1,7 @@
+﻿namespace MultiShop.WebUI.Services
+{
+    public class ILoginService
+    {
+        public string GetUserId { get; }
+    }
+}
