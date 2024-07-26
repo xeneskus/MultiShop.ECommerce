fd9fdf2f8135b647b5a59546cf23027db6305d67
xeneskus <xeneskus@gmail.com>
2024-07-26 22:20:35 +0300

    Added Identity Service

---
+++ Frontends/MultiShop.WebUI/Services/Concrete/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Services.Interfaces;
+
+namespace MultiShop.WebUI.Services.Concrete
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<bool> SignIn(SignInDto signInDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
