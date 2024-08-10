4477148dd7ca847d7492a07a411763a8bbcd58de
xeneskus <xeneskus@gmail.com>
2024-08-10 22:33:49 +0300

    Ocelot Program.cs Configuration

--- ApiGateway/MultiShop.OcelotGateway/Program.cs
+++ ApiGateway/MultiShop.OcelotGateway/Program.cs
@@ -1,6 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
+var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("Ocelot.json").Build();
+
+builder.Services.AddOcelot(configuration);
+
+
+var app = builder.Build();
+await app.UseOcelot();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
