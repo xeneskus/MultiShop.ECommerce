63cbc83b081955fbedc0831d2e88ac83563058c0
xeneskus <xeneskus@gmail.com>
2024-08-11 23:39:44 +0300

    Service Api Settings Configuration and Identity Service Url Configuration

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -39,6 +39,7 @@ builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddHttpClient<IIdentityService, IdentityService>();
 
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
+builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
