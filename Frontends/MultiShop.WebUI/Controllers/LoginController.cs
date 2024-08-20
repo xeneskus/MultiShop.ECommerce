ba088872fe284c72d31838136addcef0b70f7ff1
xeneskus <xeneskus@gmail.com>
2024-08-20 22:44:20 +0300

    Added User Detail View Model

--- Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
 using MultiShop.WebUI.Models;
-using MultiShop.WebUI.Services;
+using MultiShop.WebUI.Services.Interfaces;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
