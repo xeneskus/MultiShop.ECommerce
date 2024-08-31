aa92378d2d90116c25f5ce3af4605ec28a5d77ab
xeneskus <xeneskus@gmail.com>
2024-08-31 18:05:53 +0300

    Refactor: Moved HTTP calls to service layer in Product Detail Image Slider Component Partial

--- Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailImageSliderComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/ProductDetailViewComponents/_ProductDetailImageSliderComponentPartial.cs
@@ -1,27 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MultiShop.DtoLayer.CatalogDtos.ProductImageDtos;
-using Newtonsoft.Json;
+using MultiShop.WebUI.Services.CatalogServices.ProductImageServices;
 
 namespace MultiShop.WebUI.ViewComponents.ProductDetailViewComponents
 {
     public class _ProductDetailImageSliderComponentPartial : ViewComponent
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public _ProductDetailImageSliderComponentPartial(IHttpClientFactory httpClientFactory)
+        private readonly IProductImageService _productImageService;
+        public _ProductDetailImageSliderComponentPartial(IProductImageService productImageService)
         {
-            _httpClientFactory = httpClientFactory;
+            _productImageService = productImageService;
         }
         public async Task<IViewComponentResult> InvokeAsync(string id)
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7075/api/ProductImages/ProductImagesByProductId?id=" + id);
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<GetByIdProductImageDto>(jsonData);
-                return View(values);
-            }
-            return View();
+            var values = await _productImageService.GetByProductIdProductImageAsync(id);
+            return View(values);
         }
     }
 }
