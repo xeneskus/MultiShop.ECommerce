b87275db129de9e3b75756ac39a707625a94b99f
xeneskus <xeneskus@gmail.com>
2024-11-28 23:26:46 +0300

    Add AutoMapper configuration and GeneralMapping profile

--- Services/MultiShop.Message/Program.cs
+++ Services/MultiShop.Message/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using MultiShop.Message.DAL.Context;
+using System.Reflection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
