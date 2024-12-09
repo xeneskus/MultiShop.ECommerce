fcee43d376edd73b18c988eb5024a74f7a004c29
xeneskus <xeneskus@gmail.com>
2024-12-09 17:32:00 +0300

    Added User Mvc Controller

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        public IActionResult UserList()
+        {
+            return View();
+        }
+    }
+}
