7f802816ee7dfe6e9b5cb8feed49b860baff6517
xeneskus <xeneskus@gmail.com>
2024-12-09 22:26:58 +0300

    Added Cargo MVC Controller to Admin Area

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CargoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    public class CargoController : Controller
+    {
+        public IActionResult CargoList()
+        {
+            return View();
+        }
+    }
+}
