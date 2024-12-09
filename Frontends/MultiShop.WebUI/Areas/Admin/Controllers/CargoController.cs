d3c708c37228266689f622fd6596d5bd36c30658
xeneskus <xeneskus@gmail.com>
2024-12-09 22:37:44 +0300

    Shipping Company List (Cargo MVC Controller)

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CargoController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CargoController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CargoServices.CargoCompanyServices;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Route("Admin/Cargo")]
     public class CargoController : Controller
     {
-        public IActionResult CargoList()
+        private readonly ICargoCompanyService _cargoCompanyService;
+        public CargoController(ICargoCompanyService cargoCompanyService)
         {
-            return View();
+            _cargoCompanyService = cargoCompanyService;
         }
+
+        [Route("CargoCompanyList")]
+        public async Task<IActionResult> CargoCompanyList()
+        {
+            var values = await _cargoCompanyService.GetAllCargoCompanyAsync();
+            return View(values);
+        }
+
     }
 }
