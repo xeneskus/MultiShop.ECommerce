546dd741c76d82a4b22a967426d9976dad1aa053
xeneskus <xeneskus@gmail.com>
2024-12-12 14:04:22 +0300

    Displaying Catalog Statistics in UI

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CommentServices;
+using MultiShop.WebUI.Services.StatisticServices.CatalogStatisticServices;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
-
     public class StatisticController : Controller
     {
-        public IActionResult Index()
+        private readonly ICatalogStatisticService _catalogStatisticService;
+        public StatisticController(ICatalogStatisticService catalogStatisticService)
         {
+            _catalogStatisticService = catalogStatisticService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var getBrandCount = await _catalogStatisticService.GetBrandCount();
+            var getProductCount = await _catalogStatisticService.GetProductCount();
+            var getCategoryCount = await _catalogStatisticService.GetCategoryCount();
+            var getMaxPriceProductName = await _catalogStatisticService.GetMaxPriceProductName();
+            var getMinPriceProductName = await _catalogStatisticService.GetMinPriceProductName();
+
+       
+
+            ViewBag.getBrandCount = getBrandCount;
+            ViewBag.getProductCount = getProductCount;
+            ViewBag.getCategoryCount = getCategoryCount;
+            ViewBag.getMaxPriceProductName = getMaxPriceProductName;
+            ViewBag.getMinPriceProductName = getMinPriceProductName;
+
             return View();
         }
     }
