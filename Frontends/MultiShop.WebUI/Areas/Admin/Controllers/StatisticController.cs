8d3c2e4e13159e6c76d8748d33c87dea08e92b6b
xeneskus <xeneskus@gmail.com>
2024-12-12 13:41:52 +0300

    Statistics Web UI Prepared

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class StatisticController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
