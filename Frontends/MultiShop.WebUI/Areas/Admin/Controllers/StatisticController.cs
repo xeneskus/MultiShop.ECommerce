83e23c0ae40bf8e6b99b5c55a374735c50910b9b
xeneskus <xeneskus@gmail.com>
2024-12-12 16:42:50 +0300

    Displaying Message Statistic in UI

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/StatisticController.cs
@@ -2,6 +2,7 @@
 using MultiShop.WebUI.Services.CommentServices;
 using MultiShop.WebUI.Services.StatisticServices.CatalogStatisticServices;
 using MultiShop.WebUI.Services.StatisticServices.DiscountStatisticServices;
+using MultiShop.WebUI.Services.StatisticServices.MessageStatisticServices;
 using MultiShop.WebUI.Services.StatisticServices.UserStatisticServices;
 
 namespace MultiShop.WebUI.Areas.Admin.Controllers
@@ -13,13 +14,15 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         private readonly IUserStatisticService _userStatisticService;
         private readonly ICommentService _commentService;
         private readonly IDiscountStatisticService _discountStatisticService;
+        private readonly IMessageStatisticService _messageStatisticService;
 
-        public StatisticController(ICatalogStatisticService catalogStatisticService, IUserStatisticService userStatisticService, ICommentService commentService, IDiscountStatisticService discountStatisticService)
+        public StatisticController(ICatalogStatisticService catalogStatisticService, IUserStatisticService userStatisticService, ICommentService commentService, IDiscountStatisticService discountStatisticService, IMessageStatisticService messageStatisticService)
         {
             _catalogStatisticService = catalogStatisticService;
             _userStatisticService = userStatisticService;
             _commentService = commentService;
             _discountStatisticService = discountStatisticService;
+            _messageStatisticService = messageStatisticService;
         }
 
         public async Task<IActionResult> Index()
@@ -38,6 +41,8 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 
             var getDiscountCouponCount = await _discountStatisticService.GetDiscountCouponCount();
 
+            var getMessageTotalCount = await _messageStatisticService.GetTotalMessageCount();
+
 
             ViewBag.getBrandCount = getBrandCount;
             ViewBag.getProductCount = getProductCount;
@@ -52,6 +57,8 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             ViewBag.getPassiveCommentCount = getPassiveCommentCount;
 
             ViewBag.getDiscountCouponCount = getDiscountCouponCount;
+
+            ViewBag.getMessageTotalCount = getMessageTotalCount;
 
 
             return View();
