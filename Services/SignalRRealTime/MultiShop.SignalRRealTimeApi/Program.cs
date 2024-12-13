2fd952335621d70238f14d105c2f9c4647dc695c
xeneskus <xeneskus@gmail.com>
2024-12-13 15:57:03 +0300

    Added SignalR Comment Service

--- Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
@@ -1,3 +1,5 @@
+using MultiShop.SignalRRealTimeApi.Services.SignalRCommentServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+
+builder.Services.AddScoped<ISignalRCommentService, SignalRCommentService>();
+
 
 var app = builder.Build();
 
